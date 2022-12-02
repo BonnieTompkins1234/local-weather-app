43de40b1fc0316984e4abbb9d6af87c28b4e4398
bonnietompkins <39962663+bonnietompkins@users.noreply.github.com>
2022-12-01 23:26:59 -0500

    update modules

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: 'b229dbeafaafaf30c216f676a34b4b1c',
+  baseURL: 'http://',
 };
 
 /*
